41ec8fa5b5b89c736726e16b518897da367f177e
fp5774s <fachrialdimaspp15@gmail.com>
2023-11-22 23:03:58 +0100

    Baseball enviroment change
    baseball enviroment change
    delete unused assets

--- Assets/Scripts/Baseball/ThrowTrigger.cs
+++ Assets/Scripts/Baseball/ThrowTrigger.cs
@@ -1,60 +1,36 @@
 
+using UnityEditor.Timeline;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class ThrowTrigger : MonoBehaviour
 {
-    public Transform buttonVisual; // The visual component of the button (e.g., the button cap)
-    public float buttonPressDistance = 0.05f; // Distance the button moves when pressed
-    public bool isPressed = false; // Initial state of the button
+    public bool isActivated = false; // Initial state of the button
     [SerializeField] GameObject thrower;
+    [SerializeField] private float delayWindow = 5f;
 
-    private Vector3 initialButtonPosition;
-    private XRGrabInteractable interactable;
     private ThrowerController throwerController;
+    private float timeDelay = 0f;
 
     void Start()
     {
-        interactable = GetComponent<XRGrabInteractable>();
         throwerController = thrower.GetComponent<ThrowerController>();
-        if (interactable != null)
-        {
-            interactable.onSelectEntered.AddListener(PressButton);
-            interactable.onSelectExited.AddListener(ReleaseButton);
-        }
-
-        initialButtonPosition = buttonVisual.localPosition;
-        UpdateVisual();
+        isActivated = false;
+        timeDelay = delayWindow;
     }
-    
-    void PressButton(XRBaseInteractor interactor)
+    private void Update()
     {
-        isPressed = true;
-        UpdateVisual();
-        throwerController.ThrowDelay();
-        // Add actions when the button is pressed
+        timeDelay -= Time.deltaTime;
+        if (isActivated && timeDelay<=0)
+        {
+            timeDelay = delayWindow;
+            throwerController.ThrowBaseball();
+        }
     }
 
-    void ReleaseButton(XRBaseInteractor interactor)
+    public void PressButton()
     {
-        isPressed = false;
-        UpdateVisual();
-        // Add actions when the button is released
-    }
-
-    void UpdateVisual()
-    {
-        // Move the button visual component based on its pressed state
-        if (isPressed)
-        {
-            buttonVisual.localPosition = initialButtonPosition - Vector3.up * buttonPressDistance;
-        }
-        else
-        {
-            buttonVisual.localPosition = initialButtonPosition;
-        }
-
-        // You can also trigger other actions or visuals based on the button state change
+        isActivated = !isActivated;
     }
 
 }
