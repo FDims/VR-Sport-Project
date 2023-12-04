cf0a1903a543677271424b931ca733e11fa5f065
fp5774s <fachrialdimaspp15@gmail.com>
2023-12-04 19:27:55 +0100

    fix bat collider, doors height & + confirmation when exit

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -5,6 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    [SerializeField] private GameObject confirmationPopUp = null;
+
+    public void Start()
+    {
+        DeactivatePopUp();
+    }
+    public void DeactivatePopUp()
+    {
+        confirmationPopUp.SetActive(false);
+    }
+    public void ActivatePopUp(GameObject PopUp)
+    {
+        DeactivatePopUp();
+
+        PopUp.SetActive(true);
+    }
     public void SceneLoader(string SceneNumber)
     {
         SceneManager.LoadScene(SceneNumber);
