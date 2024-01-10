302eff7bba5fe70862a5cb0b769dda08ff734b29
alexandrmatthew <115173249+alexandrmatthew@users.noreply.github.com>
2024-01-10 13:25:49 +0100

    add score system to script

--- Assets/Scripts/RandomTarget.cs
+++ Assets/Scripts/RandomTarget.cs
@@ -6,11 +6,17 @@ public class RandomTarget : MonoBehaviour
 {
     void OnCollisionEnter(Collision collision)
     {
+       // public int scoreValue = 100; // Score value for hitting the target
+
         if (collision.gameObject.CompareTag("Bullet"))
         {
             // Handle the hit (e.g., despawn and respawn).
             Destroy(gameObject);
             FindObjectOfType<TargetSpawner>().SpawnTarget();
+
+            // Increase the score
+            // ScoreManager.Instance.AddScore(scoreValue);
+
         }
     }
 }
